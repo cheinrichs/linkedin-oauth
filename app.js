1a78b594087a7c3ef59a24fef5758914bd817370
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-03-31 20:38:37 -0600

    Added logout functionality

--- app.js
+++ app.js
@@ -70,8 +70,7 @@ passport.deserializeUser(function(user, done) {
 app.use(function (req, res, next) {
   res.locals.user = req.user
   next()
-})
-
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/logout', function(req, res, next){
+  // req.session.destroy();
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
